fae7cccd0ac3bcc7876ada492e6573e3a076449d
Taffarel Campos <taffarel_lc@hotmail.com>
2024-04-25 00:05:09 -0300

    Implementando Navegação com Jetpack e host

---
+++ app/src/main/java/com/example/serviceorder/FragmentSearch.kt
@@ -0,0 +1,18 @@
+package com.example.serviceorder
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentSearch : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container,false)
+    }
+}
