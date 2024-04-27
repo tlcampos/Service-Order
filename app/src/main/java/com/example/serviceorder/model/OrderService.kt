f0265d1542c51faf65afbae1c6a2355092702253
Taffarel Campos <taffarel_lc@hotmail.com>
2024-04-26 23:53:18 -0300

    Criando lista de OS

---
+++ app/src/main/java/com/example/serviceorder/model/OrderService.kt
@@ -0,0 +1,8 @@
+package com.example.serviceorder.model
+
+data class OrderService(
+    val orderNumber: String,
+    val orderDataStart: String,
+    val orderDevice: String,
+    val orderResume: String
+)
