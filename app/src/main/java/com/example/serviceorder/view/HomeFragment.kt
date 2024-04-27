3f8ad53d7c46618b28d35d7ad0b6dd11ff01b61f
Taffarel Campos <taffarel_lc@hotmail.com>
2024-04-27 00:23:50 -0300

    Criando lista de OS

---
+++ app/src/main/java/com/example/serviceorder/view/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.example.serviceorder.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.serviceorder.R
+import com.example.serviceorder.model.OrderService
+import com.xwray.groupie.GroupieAdapter
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_main)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val adapter = GroupieAdapter()
+        recyclerView.adapter = adapter
+
+        adapter.add(OrderServiceItem(OrderService("OS:000001","26/abril","Smartphone","O celular não carrega após queda, Necessita a troca")))
+    }
+}
---
+++ app/src/main/java/com/example/serviceorder/view/OrderServiceItem.kt
@@ -0,0 +1,24 @@
+package com.example.serviceorder.view
+
+import android.view.View
+import android.widget.TextView
+import com.example.serviceorder.R
+import com.example.serviceorder.model.OrderService
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+
+class OrderServiceItem(private val orderService: OrderService) : Item<OrderServiceItem.OrderServiceViewHolder>() {
+
+    class OrderServiceViewHolder(view: View) : GroupieViewHolder(view)
+
+    override fun createViewHolder(itemView: View) = OrderServiceViewHolder(itemView)
+
+    override fun bind(viewHolder: OrderServiceViewHolder, position: Int) {
+        viewHolder.itemView.findViewById<TextView>(R.id.order_number).text = orderService.orderNumber
+        viewHolder.itemView.findViewById<TextView>(R.id.order_data_open).text = orderService.orderDataStart
+        //viewHolder.itemView.findViewById<TextView>(R.id.order_device).text = orderService.orderDevice
+        viewHolder.itemView.findViewById<TextView>(R.id.order_resume).text = orderService.orderResume
+    }
+
+    override fun getLayout() = R.layout.order_service_item
+}
