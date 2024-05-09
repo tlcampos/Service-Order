22441c6809bbbbb2248a61b81e09dff7f84cf510
Taffarel Campos <taffarel_lc@hotmail.com>
2024-05-08 23:37:16 -0300

    Adicionando Firebase ao projeto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.serviceorder"
-        minSdk = 22
+        minSdk = 23
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
